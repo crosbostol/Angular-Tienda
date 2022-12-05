0a65179461f28985cbee4dd1db262864283a4df9
crosbostol <89926514+crosbostol@users.noreply.github.com>
2022-12-05 01:44:53 -0300

    Add files via upload

---
+++ Tienda angular/src/app/cart.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+
+export type ShippingPrice = {
+  type: string;
+  price: number;
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  items: Product[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearItems() {
+    this.items = [];
+
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<ShippingPrice[]>('/assets/shipping.json');
+  }
+}
